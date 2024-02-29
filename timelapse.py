3c75f9d3f531af5380f8815f5f3acd8600950111
H. Can Celik <celik@me.com>
2024-02-29 10:41:14 +0300

    Improve logging

--- timelapse.py
+++ timelapse.py
@@ -1,10 +1,11 @@
 from os import system
+from os import path
 from datetime import datetime as dt
 from time import sleep
 from picamera import PiCamera
 
 # Variables to change
-imagesPath = 'home/pi/timelapse/images'
+path = 'home/pi/timelapse'
 tlminutes = 60 * 24 #set this to the number of minutes you wish to run your timelapse camera
 secondsinterval = 60 * 1 #number of seconds delay between each photo taken
 fps = 30 #frames per second timelapse video
@@ -13,9 +14,12 @@ resolution = (1280, 720)
 initialSleep = 60 * 5 #number of seconds to sleep before starting the timelapse
 
 # No need to change anything below this line
+print(f'{dt.now().strftime("%Y-%m-%d %H:%M:%S")} Starting timelapse...')
+
+imagesPath = f'{path}/images'
 
 # Check if this script is running on a Raspberry Pi
-if os.path.exists(f'{imagesPath}/running.txt'):
+if path.exists(f'{path}/running.txt'):
     print("Another instance of the script is running. Exiting...")
     exit()
 
@@ -23,11 +27,11 @@ if os.path.exists(f'{imagesPath}/running.txt'):
 sleep(initialSleep)
 
 # Create a file to indicate that the script is running
-system(f'touch {imagesPath}/running.txt')
+system(f'touch {path}/running.txt')
 
 # Calculate the number of photos to take
 numphotos = int((tlminutes*60)/secondsinterval)
-print("Number of photos to take = ", numphotos)
+print(f'{dt.now().strftime("%Y-%m-%d %H:%M:%S")} Number of photos to take = {numphotos}')
 
 camera = PiCamera()
 camera.rotation = rotation
@@ -37,4 +41,4 @@ for i in range(numphotos):
     camera.capture(f'{imagesPath}/image-{dt.now().strftime("%Y%m%d-%H%M%S")}.jpg')
     sleep(secondsinterval)
 
-print("Done timelapse.")
+print(f'{dt.now().strftime("%Y-%m-%d %H:%M:%S")} Done timelapse.')
